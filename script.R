06923332fb3b450158e0239d9bafb99cb218a91c
Allan Cueff <allan.cueff@protonmail.com>
2024-04-09 11:11:43 +0200

    select column and row + average value

--- script.R
+++ script.R
@@ -1,4 +1,5 @@
-downloadedCSV <- gzcon(url(paste("https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/BASE/QUOT/",
-                       "Q_44_previous-1950-2022_RR-T-Vent.csv.gz", sep="")))
-txt <- readLines(downloadedCSV)
-dat <- read.csv(textConnection(txt))
+library(tidyverse)
+
+downloadedCSV <- gzcon(url("https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/BASE/QUOT/Q_44_previous-1950-2022_RR-T-Vent.csv.gz"))
+dat <- read.csv(textConnection(readLines(downloadedCSV)), sep=";")
+dat %>% select(AAAAMMJJ, RR)
