3bdb6f05e782a857d8c55cab1e68e4d7534f7c99
Allan Cueff <allan.cueff@protonmail.com>
2024-04-09 11:38:03 +0200

    forgot to save this

--- script.R
+++ script.R
@@ -2,4 +2,4 @@ library(tidyverse)
 
 downloadedCSV <- gzcon(url("https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/BASE/QUOT/Q_44_previous-1950-2022_RR-T-Vent.csv.gz"))
 dat <- read.csv(textConnection(readLines(downloadedCSV)), sep=";")
-dat %>% select(AAAAMMJJ, RR)
+dat %>% select(AAAAMMJJ, RR)  %>% filter(AAAAMMJJ == "20170423") %>% drop_na() %>% summarize(across(everything(), mean))
