bab96d50fa7b3b9342592e9c5ec7a86a691421e6
Allan Cueff <allan.cueff@protonmail.com>
2024-04-09 12:10:16 +0200

    get average value

--- script.R
+++ script.R
@@ -2,4 +2,5 @@ library(tidyverse)
 
 downloadedCSV <- gzcon(url("https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/BASE/QUOT/Q_44_previous-1950-2022_RR-T-Vent.csv.gz"))
 dat <- read.csv(textConnection(readLines(downloadedCSV)), sep=";")
-dat %>% select(AAAAMMJJ, RR)  %>% filter(AAAAMMJJ == "20170423") %>% drop_na() %>% summarize(across(everything(), mean))
+result = dat %>% filter(AAAAMMJJ == "20170423") %>% select(RR)  %>% drop_na() %>% summarize(across(everything(), mean))
+value = result[[1]]
