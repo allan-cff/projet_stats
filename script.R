8720066d3d73cebfbfd83856076899238627dfe3
Maxime Chauveau <maxchauveau44@gmail.com>
2024-04-10 20:29:34 +0200

    Matrice fonctionnelle avec commentaires

--- script.R
+++ script.R
@@ -1,20 +1,20 @@
 library(tidyverse)
 
-departement <- c(44, 29, 61, 33, 13, 69, 75, 59, 37, 67)
-jour <- c("20020421", "20020505", "20070422", "20070506", "20120422", "20120506", "20170421", "20170507", "20220410", "20220424")
+departement <- c(44, 29, 61, 33, 13, 69, 75, 59, 37, 67) #departments
+jour <- c("20020421", "20020505", "20070422", "20070506", "20120422", "20120506", "20170421", "20170507", "20220410", "20220424") # date election
 matrice <- matrix(nrow = length(departement), ncol = length(departement))
 
 for (i in seq_along(departement)) {
   
   downloadedCSV <- gzcon(url(paste("https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/BASE/QUOT/",
-                                   "Q_", departement[i], "_previous-1950-2022_RR-T-Vent.csv.gz", sep="")))
-  dat <- read.csv(textConnection(readLines(downloadedCSV)), sep=";")
+                                   "Q_", departement[i], "_previous-1950-2022_RR-T-Vent.csv.gz", sep="")))         # téléchargement fichier pluie pour un departement
+  dat <- read.csv(textConnection(readLines(downloadedCSV)), sep=";")  # lecture du csv
   
   h=1
   for(j in jour){
-    tmp <- dat %>% filter(AAAAMMJJ == j) %>% select(RR) %>% drop_na() %>% summarize(across(everything(), mean))
-    matrice[i, h] <- tmp[[1]]
-    h=h+1
+    tmp <- dat %>% filter(AAAAMMJJ == j) %>% select(RR) %>% drop_na() %>% summarize(across(everything(), mean))   #récupère la moyenne pour un jour
+    matrice[i, h] <- tmp[[1]]     #remplissage matrice avec la valeur voulue
+    h=h+1         #iteration moche
   }
   
   
